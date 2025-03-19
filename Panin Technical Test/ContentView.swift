67e4dee6ef43a6e97f39b13fdb2afd2832f5b88d
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2025-03-19 23:40:10 +0700

    Initial Commit

---
+++ Panin Technical Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Panin Technical Test
+//
+//  Created by Bryan Vernanda on 19/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
