a0889cbf7308ce5fadeb8ea6cdee74e0efa418de
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2025-03-20 08:57:05 +0700

    feat: create watchlist usecases ~ continue

---
+++ Panin Technical Test/Domain/UseCases/GetWatchlistUnselectedStocks.swift
@@ -0,0 +1,25 @@
+//
+//  GetWatchlistUnselectedStocks.swift
+//  Technical Test
+//
+//  Created by Bryan Vernanda on 18/03/25.
+//
+
+import Foundation
+
+protocol GetWatchlistUnselectedStocks {
+    func execute(allStocks: [Stock], watchlistStocks: [Stock]) -> [Stock]
+}
+
+struct GetWatchlistUnselectedStocksImpl: GetWatchlistUnselectedStocks {
+    func execute(allStocks: [Stock], watchlistStocks: [Stock]) -> [Stock] {
+        let watchlistSymbols = Set(watchlistStocks.map { $0.symbol })
+        return allStocks
+                .filter { !watchlistSymbols.contains($0.symbol) }
+                .map { stock in
+                    var modifiedStock = stock
+                    modifiedStock.description = StockDescription(rawValue: stock.symbol)?.value
+                    return modifiedStock
+                }
+    }
+}
---
+++ Panin Technical Test/Domain/UseCases/GetWatchlists.swift
@@ -0,0 +1,38 @@
+//
+//  GetWatchlists.swift
+//  Technical Test
+//
+//  Created by Bryan Vernanda on 18/03/25.
+//
+
+import Foundation
+
+protocol GetWatchlists {
+    func execute(stocks: [Stock]) -> [Stock]
+}
+
+struct GetWatchlistsImpl: GetWatchlists {
+    let watchlistRepo: WatchlistRepository
+
+    func execute(stocks: [Stock]) -> [Stock] {
+        let watchlistResult = watchlistRepo.fetchAll()
+        
+        switch watchlistResult {
+        case .success(let watchlists):
+            let watchlistNames = watchlists.map { $0.watchlistName }
+            
+            return stocks
+                .filter { watchlistNames.contains($0.symbol) }
+                .map { stock in
+                    var modifiedStock = stock
+                    modifiedStock.description = StockDescription(rawValue: stock.symbol)?.value
+                    return modifiedStock
+                }
+            
+        case .failure(let error):
+            debugPrint("Error fetching watchlists: \(error)")
+            return []
+        }
+    }
+}
+
