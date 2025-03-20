46ece98d88e2762e86446fd7b1b267688601792f
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2025-03-20 16:27:43 +0700

    fix: add a loading state when the stocks data hasn't loaded yet

--- Panin Technical Test/Presentation/Stock/Views/StockView.swift
+++ Panin Technical Test/Presentation/Stock/Views/StockView.swift
@@ -10,72 +10,82 @@ import SwiftUI
 struct StockView: View {
     let router: any AppRouterProtocol
     @StateObject private var viewModel = StockViewModel()
-
+    
     var body: some View {
-        HStack {
-            Text("Stock List")
-            
-            Image(systemName: "chart.line.text.clipboard")
-        }
-        .fontWeight(.bold)
-        .font(.title)
-        .padding(.bottom, 32)
-        .padding(.top, 16)
-        .padding(.horizontal)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .onAppear {
-            Task {
-                await viewModel.loadStocks()
-            }
-        }
-        
-        ScrollView(.horizontal, showsIndicators: false) {
-            VStack(alignment: .leading) {
-                // Header Row
-                HStack {
-                    Text("STOCK").bold().frame(width: 80, alignment: .leading)
-                    Text("VOL").bold().frame(width: 80, alignment: .leading)
-                    Text("FRQ").bold().frame(width: 80, alignment: .leading)
-                    Text("PREV").bold().frame(width: 80, alignment: .leading)
-                    Text("+/-").bold().frame(width: 80, alignment: .leading)
-                    Text("%").bold().frame(width: 80, alignment: .leading)
-                    Text("PRICE").bold().frame(width: 80, alignment: .leading)
-                }
-                .padding(.bottom, 4)
-                .foregroundStyle(.orange)
+        VStack(spacing: 0) {
+            HStack {
+                Text("Stock List")
                 
-                // List of Stocks
-                List {
-                    ForEach(viewModel.stocks, id: \.symbol) { stock in
-                        VStack {
-                            HStack {
-                                Text(stock.symbol)
-                                    .frame(width: 80, alignment: .leading)
-                                Text(stock.volume)
-                                    .frame(width: 80, alignment: .leading)
-                                Text(stock.frequency)
-                                    .frame(width: 80, alignment: .leading)
-                                Text("\(stock.previous)")
-                                    .frame(width: 80, alignment: .leading)
-                                Text((stock.change > 0 ? "+" : "") + "\(stock.change)")
-                                    .foregroundStyle(stock.change.profitLossColor())
-                                    .frame(width: 80, alignment: .leading)
-                                Text(stock.percentage)
-                                    .foregroundStyle(stock.change.profitLossColor())
-                                    .frame(width: 80, alignment: .leading)
-                                Text("\(stock.price)")
-                                    .foregroundStyle(stock.change.profitLossColor())
-                                    .frame(width: 80, alignment: .leading)
-                            }
-                            Divider()
-                        }
-                        .listRowSeparator(.hidden)
-                    }
-                }
-                .listStyle(.plain)
+                Image(systemName: "chart.line.text.clipboard")
             }
+            .fontWeight(.bold)
+            .font(.title)
+            .padding(.bottom, 32)
+            .padding(.top)
             .padding(.horizontal)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .onAppear {
+                Task {
+                    await viewModel.loadStocks()
+                }
+            }
+            
+            
+            if viewModel.stocks.isEmpty {
+                Text("Loading..")
+                    .font(.title3)
+                    .frame(maxHeight: .infinity, alignment: .center)
+            } else {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    VStack(alignment: .leading) {
+                        // Header Row
+                        HStack {
+                            Text("STOCK").bold().frame(width: 80, alignment: .leading)
+                            Text("VOL").bold().frame(width: 80, alignment: .leading)
+                            Text("FRQ").bold().frame(width: 80, alignment: .leading)
+                            Text("PREV").bold().frame(width: 80, alignment: .leading)
+                            Text("+/-").bold().frame(width: 80, alignment: .leading)
+                            Text("%").bold().frame(width: 80, alignment: .leading)
+                            Text("PRICE").bold().frame(width: 80, alignment: .leading)
+                        }
+                        .padding(.bottom, 4)
+                        .foregroundStyle(.orange)
+                        
+                        // List of Stocks
+                        List {
+                            ForEach(viewModel.stocks, id: \.symbol) { stock in
+                                VStack {
+                                    HStack {
+                                        Text(stock.symbol)
+                                            .frame(width: 80, alignment: .leading)
+                                        Text(stock.volume)
+                                            .frame(width: 80, alignment: .leading)
+                                        Text(stock.frequency)
+                                            .frame(width: 80, alignment: .leading)
+                                        Text("\(stock.previous)")
+                                            .frame(width: 80, alignment: .leading)
+                                        Text((stock.change > 0 ? "+" : "") + "\(stock.change)")
+                                            .foregroundStyle(stock.change.profitLossColor())
+                                            .frame(width: 80, alignment: .leading)
+                                        Text(stock.percentage)
+                                            .foregroundStyle(stock.change.profitLossColor())
+                                            .frame(width: 80, alignment: .leading)
+                                        Text("\(stock.price)")
+                                            .foregroundStyle(stock.change.profitLossColor())
+                                            .frame(width: 80, alignment: .leading)
+                                    }
+                                    Divider()
+                                }
+                                .listRowSeparator(.hidden)
+                            }
+                        }
+                        .listStyle(.plain)
+                    }
+                    .padding(.horizontal)
+                }
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
 
